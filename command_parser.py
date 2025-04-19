5f81e4b6d7636549bc4c837383cadca8ede161f2
lkennedy <lpkenned@uci.edu>
2025-04-18 22:14:54 -0700

    More docstrings

--- command_parser.py
+++ command_parser.py
@@ -9,10 +9,13 @@
 import shlex
 
 def parse_command(command_str):
+    """Runs a shell stype command string into a command dictionary.
+    Uses the command_str as the input.
+    Will return dictionary with types and args."""
     try:
         separate = shlex.split(command_str)
         if not separate:
-            raise ValueError("Empty command.")
+            raise ValueError("Empty command")
         return {"type": separate[0], "args": separate[1:]}
     except Exception:
-        raise ValueError("Failed to parse command.")
+        raise ValueError("Failed to parse command")
